0c76a731e7d834c8543c3b89b33500b6a46b3bc3
markcmurphy <mark@murphymark.me>
2020-03-20 16:17:52 -0500

    displaying info from express

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,37 +1,61 @@
-import React, { Component } from 'react';
-import Layout from '../components/layout';
-import Intro from '../components/mask';
-import SEO from '../components/seo';
-import Carousel from '../components/carousel';
+import React, { Component } from 'react'
+import Layout from '../components/layout'
+import Intro from '../components/mask'
+import SEO from '../components/seo'
+import Carousel from '../components/carousel'
 import AboutSection from '../components/aboutSection'
 import Card from '../components/card'
 import { MDBRow } from 'mdbreact'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { apiResponse: '' }
+  }
+
+  callAPI() {
+    fetch('http://localhost:3000/testAPI')
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }))
+  }
+
+  componentWillMount() {
+    this.callAPI()
+  }
+
   render() {
     return (
       <>
         <Layout>
-          <SEO title="Home" keywords={[`gatsby`, `MDBReact`, `react`, `Material Design For Bootstrap`]} />
-        <Carousel />
-        <Intro />
-        <main>
-          <AboutSection />
-          <section id="cardSection">
-            <h2 className="h1-responsive text-center font-weight-bold mb-5">
-              Our bestsellers
-            </h2>
-            <MDBRow className="m-0" center>
-              <Card />
-              <Card />
-              <Card />
-            </MDBRow>
-          </section>
-        </main>
+          <SEO
+            title="Home"
+            keywords={[
+              `gatsby`,
+              `MDBReact`,
+              `react`,
+              `Material Design For Bootstrap`,
+            ]}
+          />
+          <Carousel />
+          <Intro />
+          <main>
+            <AboutSection />
+            <section id="cardSection">
+              <h2 className="h1-responsive text-center font-weight-bold mb-5">
+                Our bestsellers
+                <p className="App-intro">{this.state.apiResponse}</p>
+              </h2>
+              <MDBRow className="m-0" center>
+                <Card />
+                <Card />
+                <Card />
+              </MDBRow>
+            </section>
+          </main>
         </Layout>
       </>
-    );
+    )
   }
 }
 
-export default App;
+export default App
